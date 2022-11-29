23aa1ecf6ba8e4681eb28c67af100de3549c8a03
Tanvi Pandey <61287317+tannypandey@users.noreply.github.com>
2022-11-29 18:06:40 +0000

    new trial

---
+++ newBiRNN.py
@@ -0,0 +1,502 @@
+#!/usr/bin/env python3
+import time
+from multiprocessing import cpu_count
+from typing import Union, NamedTuple
+import dataset
+import evaluation
+
+
+import torch
+import torch.backends.cudnn
+import numpy as np
+from torch import nn, optim
+from torch.nn import functional as F
+import torchvision.datasets
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+
+import argparse
+from pathlib import Path
+
+torch.backends.cudnn.benchmark = True
+
+parser = argparse.ArgumentParser(
+    description="Train a simple CNN on CIFAR-10",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+default_dataset_dir = Path.home() / ".cache" / "torch" / "datasets"
+parser.add_argument("--dataset-root", default=default_dataset_dir)
+parser.add_argument("--log-dir", default=Path("logs"), type=Path)
+parser.add_argument("--learning-rate", default=0.00005, type=float, help="Learning rate")
+
+parser.add_argument(
+    "--batch-size",
+    default=64,
+    type=int,
+    help="Number of audios within each mini-batch",
+)
+parser.add_argument(
+    "--epochs",
+    default=200,
+    type=int,
+    help="Number of epochs (passes through the entire dataset) to train for",
+)
+parser.add_argument(
+    "--val-frequency",
+    default=2,
+    type=int,
+    help="How frequently to test the model on the validation set in number of epochs",
+)
+parser.add_argument(
+    "--log-frequency",
+    default=10,
+    type=int,
+    help="How frequently to save logs to tensorboard in number of steps",
+)
+parser.add_argument(
+    "--print-frequency",
+    default=10,
+    type=int,
+    help="How frequently to print progress to the command line in number of steps",
+)
+parser.add_argument(
+    "-j",
+    "--worker-count",
+    default=cpu_count(),
+    type=int,
+    help="Number of worker processes used to load data.",
+)
+
+
+class AudioShape(NamedTuple):
+    height: int
+    width: int
+    channels: int
+
+
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
+
+
+def main(args):
+    transform = transforms.ToTensor()
+    args.dataset_root.mkdir(parents=True, exist_ok=True)
+    train_dataset = dataset.GTZAN("train.pkl")
+    test_dataset = dataset.GTZAN("val.pkl")
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        shuffle=True,
+        batch_size=args.batch_size,
+        pin_memory=True,
+        num_workers=args.worker_count,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        shuffle=False,
+        batch_size=args.batch_size,
+        num_workers=args.worker_count,
+        pin_memory=True,
+    )
+
+    model = CNN(height=80, width=80, channels=1, class_count=10)
+    ## TASK 8: Redefine the criterion to be softmax cross entropy
+    criterion = nn.CrossEntropyLoss()
+
+
+    ## TASK 11: Define the optimizer
+    optimizer = torch.optim.Adam(model.parameters(),lr=args.learning_rate,betas=(0.9,0.999),eps=1e-08)
+    #optimizer = torch.optim.SGD(model.parameters(),lr=args.learning_rate)
+
+    log_dir = get_summary_writer_log_dir(args)
+    print(f"Writing logs to {log_dir}")
+    summary_writer = SummaryWriter(
+            str(log_dir),
+            flush_secs=5
+    )
+    trainer = Trainer(
+        model, train_loader, test_loader, criterion, optimizer,summary_writer, DEVICE
+    )
+
+    trainer.train(
+        args.epochs,
+        args.val_frequency,
+        print_frequency=args.print_frequency,
+        log_frequency=args.log_frequency,
+    )
+
+    summary_writer.close()
+
+
+
+class CNN(nn.Module):
+    def __init__(self, height: int, width: int, channels: int, class_count: int):
+        super().__init__()
+        self.input_shape = AudioShape(height=height, width=width, channels=channels)
+        self.class_count = class_count
+
+        # First convolution layer
+        self.conv1 = nn.Conv2d(
+            in_channels=self.input_shape.channels,
+            out_channels=16,
+            kernel_size=(10,23),
+            padding='same'
+        )
+        # Initialse the layer weights and bias
+        self.initialise_layer(self.conv1)
+
+        # First max pooling layer (after first convolution)
+        self.max_pool1 = nn.MaxPool2d(
+            kernel_size=(2, 2),
+            
+        )
+
+        self.conv1b = nn.Conv2d(
+            in_channels=self.input_shape.channels,
+            out_channels=16,
+            kernel_size=(21,10),
+            padding='same'
+        )
+        self.initialise_layer(self.conv1b)
+
+        self.max_pool1b = nn.MaxPool2d(
+            kernel_size=(2, 2),
+           
+        )
+
+        # Second convolutional layer
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=(5,11),
+            padding='same'
+        )
+        # Initialse the layer weights and bias
+        self.initialise_layer(self.conv2)
+
+        # First max pooling layer (after first convolution)
+        self.max_pool2 = nn.MaxPool2d(
+            kernel_size=(2, 2),
+            
+        )
+
+        self.conv2b = nn.Conv2d(
+            in_channels=16,
+            out_channels=32,
+            kernel_size=(10,5),
+            padding='same'
+        )
+        self.initialise_layer(self.conv2b)
+
+        self.max_pool2b = nn.MaxPool2d(
+            kernel_size=(2, 2),
+           
+        )
+
+        # Third convolutional layer
+        self.conv3 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(3,5),
+            padding='same'
+        )
+        # Initialse the layer weights and bias
+        self.initialise_layer(self.conv3)
+
+        # First max pooling layer (after first convolution)
+        self.max_pool3 = nn.MaxPool2d(
+            kernel_size=(2, 2),
+            
+        )
+
+        self.conv3b = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(5,3),
+            padding='same'
+        )
+        self.initialise_layer(self.conv3b)
+
+        self.max_pool3b = nn.MaxPool2d(
+            kernel_size=(2, 2),
+           
+        )
+
+        # Fourth Convolutional layer
+        self.conv4 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=(2,4),
+            padding='same'
+        )
+        # Initialse the layer weights and bias
+        self.initialise_layer(self.conv4)
+
+        # First max pooling layer (after first convolution)
+        self.max_pool4 = nn.MaxPool2d(
+            kernel_size=(1, 5),
+            
+        )
+
+        self.conv4b = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=(4,2),
+            padding='same'
+        )
+        self.initialise_layer(self.conv4b)
+
+        self.max_pool4b = nn.MaxPool2d(
+            kernel_size=(5, 1),
+           
+        )
+
+        # First fully connected layer
+        self.full_connect1 = nn.Linear(5520, 200)
+        self.initialise_layer(self.full_connect1)
+
+        #dropout
+        self.dropout = nn.Dropout(0.25)
+
+        # Second fully connected layer
+        self.full_connect2 = nn.Linear(200,10)
+        self.initialise_layer(self.full_connect2)
+
+        self.linear = nn.Linear(40, 20)
+
+    def BiRnn(self, audios: torch.Tensor) -> torch.Tensor:
+        y = self.max_pool1(audios)
+        y = torch.flatten(y, start_dim=1)
+        rnn = nn.GRU(input_size=1600,hidden_size=200,num_layers=1,dropout=0.25,bidirectional=True)
+        y,h=rnn(y)
+        return y
+
+
+
+    #computes the forward pass through all network layers
+    def forward(self, audios: torch.Tensor) -> torch.Tensor:
+        self.BiRnn(audios)
+        # first convolutional pipeline
+        x = self.conv1(audios)
+        x = F.leaky_relu(x, 0.3)
+        x = self.max_pool1(x)
+        #print("x shape is",x.shape)
+        x = F.relu(self.conv2(x))
+        x = self.max_pool2(x)
+        #print("x shape after 2 is",x.shape)
+        x = F.relu(self.conv3(x))
+        x = self.max_pool3(x)
+        #print("x shape after 3 is",x.shape)
+        x = F.relu(self.conv4(x))
+        x = self.max_pool4(x)
+        #print("x shape after 4 is",x.shape)
+
+        # second convolution pipeline
+        xb = self.conv1b(audios)
+        xb = F.leaky_relu(xb,0.3)
+        xb = self.max_pool1b(xb)
+        xb = F.relu(self.conv2b(xb))
+        xb = self.max_pool2b(xb)
+        xb = F.relu(self.conv3b(xb))
+        xb = self.max_pool3b(xb)
+        xb = F.relu(self.conv4b(xb))
+        xb = self.max_pool4b(xb)
+
+        x = torch.flatten(x, start_dim=1)
+        xb = torch.flatten(xb,start_dim=1)
+        # print(x.shape)
+        # print(xb.shape)
+  
+        # merge the two output
+        x = torch.cat((x,xb),1)
+        x = torch.cat((x,self.BiRnn(audios)),1)
+        #print (x.shape)
+
+        #fully connected layer
+        x = self.full_connect1(x)
+        # print("The size is", x.shape)
+
+        # dropout
+        x = self.dropout(x)
+
+        # #final fully connected layer
+        x = F.leaky_relu(x,0.3)
+        x = self.full_connect2(x)
+        
+        # print(x)
+        # print("The sum is",sum(x))
+
+        # print("After softmax",x)
+        # print("The sum is",sum(x))
+        return x
+
+    @staticmethod
+    def initialise_layer(layer):
+        if hasattr(layer, "bias"):
+            nn.init.zeros_(layer.bias)
+        if hasattr(layer, "weight"):
+            nn.init.kaiming_normal_(layer.weight)
+
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: nn.Module,
+        train_loader: DataLoader,
+        val_loader: DataLoader,
+        criterion: nn.Module,
+        optimizer: Optimizer,
+        summary_writer: SummaryWriter,
+        device: torch.device,
+    ):
+        self.model = model.to(device)
+        self.device = device
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.summary_writer = summary_writer
+        self.step = 0
+
+    def train(
+        self,
+        epochs: int,
+        val_frequency: int,
+        print_frequency: int = 20,
+        log_frequency: int = 5,
+        start_epoch: int = 0
+    ):
+        self.model.train()
+        for epoch in range(start_epoch, epochs):
+            self.model.train()
+            data_load_start_time = time.time()
+
+            for _, batch, labels, _ in (self.train_loader):
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                data_load_end_time = time.time()
+
+                logits = self.model.forward(batch)
+            
+
+                ## loss
+                weights = torch.cat([p.view(-1) for n, p in self.model.named_parameters() if ".weight" in n])
+                
+                l1_loss = 0.00001 * torch.norm(weights,1)
+                loss = self.criterion(logits,labels) + l1_loss
+
+                ## TASK 10: Compute the backward pass
+                loss.backward()
+
+                ## TASK 12: Step the optimizer and then zero out the gradient buffers.
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+
+                with torch.no_grad():
+                    preds = logits.argmax(-1)
+                    accuracy = compute_accuracy(labels, preds)
+
+                data_load_time = data_load_end_time - data_load_start_time
+                step_time = time.time() - data_load_end_time
+                if ((self.step + 1) % log_frequency) == 0:
+                    self.log_metrics(epoch, accuracy, loss, data_load_time, step_time)
+                if ((self.step + 1) % print_frequency) == 0:
+                    self.print_metrics(epoch, accuracy, loss, data_load_time, step_time)
+
+                self.step += 1
+                data_load_start_time = time.time()
+
+            self.summary_writer.add_scalar("epoch", epoch, self.step)
+            if ((epoch + 1) % val_frequency) == 0:
+                self.validate()
+                # self.validate() will put the model in validation mode,
+                # so we have to switch back to train mode afterwards
+                self.model.train()
+
+    def print_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        epoch_step = self.step % len(self.train_loader)
+        print(
+                f"epoch: [{epoch}], "
+                f"step: [{epoch_step}/{len(self.train_loader)}], "
+                f"batch loss: {loss:.5f}, "
+                f"batch accuracy: {accuracy * 100:2.2f}, "
+                f"data load time: "
+                f"{data_load_time:.5f}, "
+                f"step time: {step_time:.5f}"
+        )
+
+    def log_metrics(self, epoch, accuracy, loss, data_load_time, step_time):
+        self.summary_writer.add_scalar("epoch", epoch, self.step)
+        self.summary_writer.add_scalars(
+                "accuracy",
+                {"train": accuracy},
+                self.step
+        )
+        self.summary_writer.add_scalars(
+                "loss",
+                {"train": float(loss.item())},
+                self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", data_load_time, self.step
+        )
+        self.summary_writer.add_scalar(
+                "time/data", step_time, self.step
+        )
+
+    def validate(self):
+        results = {"preds": []}
+        total_loss = 0
+        self.model.eval()
+        # No need to track gradients for validation, we're not optimizing.
+        with torch.no_grad():
+            for _ ,batch,labels, _ in (self.val_loader):
+                batch = batch.to(self.device)
+                labels = labels.to(self.device)
+                logits = self.model(batch)
+                loss = self.criterion(logits, labels)
+                total_loss += loss.item()
+                results["preds"].extend(list(logits))
+        evaluation.evaluate(results["preds"],"val.pkl")
+
+
+def compute_accuracy(
+    labels: Union[torch.Tensor, np.ndarray], preds: Union[torch.Tensor, np.ndarray]
+) -> float:
+    """
+    Args:
+        labels: ``(batch_size, class_count)`` tensor or array containing example labels
+        preds: ``(batch_size, class_count)`` tensor or array containing model prediction
+    """
+    assert len(labels) == len(preds)
+    return float((labels == preds).sum()) / len(labels)
+
+
+def get_summary_writer_log_dir(args: argparse.Namespace) -> str:
+    """Get a unique directory that hasn't been logged to before for use with a TB
+    SummaryWriter.
+
+    Args:
+        args: CLI Arguments
+
+    Returns:
+        Subdirectory of log_dir with unique subdirectory name to prevent multiple runs
+        from getting logged to the same TB log directory (which you can't easily
+        untangle in TB).
+    """
+    tb_log_dir_prefix = f'CNN_bs={args.batch_size}_lr={args.learning_rate}_run_'
+    i = 0
+    while i < 1000:
+        tb_log_dir = args.log_dir / (tb_log_dir_prefix + str(i))
+        if not tb_log_dir.exists():
+            return str(tb_log_dir)
+        i += 1
+    return str(tb_log_dir)
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
